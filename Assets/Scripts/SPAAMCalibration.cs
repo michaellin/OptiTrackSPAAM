9f2d27b668c44742aafe51bf628767c3bd2aa7fa
Michael Lin <michaelv03@gmail.com>
2017-06-28 18:49:08 -0700

    wrote the SVD calibration fundtion.

--- Assets/Scripts/SPAAMCalibration.cs
+++ Assets/Scripts/SPAAMCalibration.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.VR.WSA.Input;
 using UnityEngine.UI;
 
-using MathNet.Numerics.LinearAlgebra;
+using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+using Vector = MathNet.Numerics.LinearAlgebra.Vector<double>;
 
 
 public class SPAAMCalibration : MonoBehaviour
@@ -24,19 +25,19 @@ public class SPAAMCalibration : MonoBehaviour
     // *** Calibration Parameters *** //
     private int numData = 12;
     private float[,] AlignmentPoints;
-    private Vector3[] CalibrationPoints =
-        { new Vector3( 0, 0, 0 ),
-          new Vector3( 0, 0, 0.5f),
-          new Vector3( 0, 0, 0.2f),
-          new Vector3( 0.6f, 0, 0.1f),
-          new Vector3( -0.5f, 0, 0.5f),
-          new Vector3( -0.3f, 0, 0.5f),
-          new Vector3( -0.8f, 0, 0.5f),
-          new Vector3( -0.1f, 0, 0.5f),
-          new Vector3( -0.3f, 0, -0.5f),
-          new Vector3( -0.8f, 0, -0.5f),
-          new Vector3( 0.7f, 0, 0.5f),
-          new Vector3( -0.05f, 0, 0.5f)
+    private float[,] CalibrationPoints =
+        { { 0, 0, 0 },
+          { 0, 0, 0.5f },
+          { 0, 0, 0.2f },
+          { 0.6f, 0, 0.1f },
+          { -0.5f, 0, 0.5f },
+          { -0.3f, 0, 0.5f },
+          { -0.8f, 0, 0.5f },
+          { -0.1f, 0, 0.5f },
+          { -0.3f, 0, -0.5f },
+          { -0.8f, 0, -0.5f },
+          { 0.7f, 0, 0.5f },
+          { -0.05f, 0, 0.5f }
         };
     private int currStep = 0;
     private Transform CalibrationObjectTransform;
@@ -50,22 +51,25 @@ public class SPAAMCalibration : MonoBehaviour
         AlignmentObjectTransform = AlignmentObject.transform;
         AlignmentObjectHMDTransform = AlignmentObjectHMD.transform;
 
-        CalibrationObjectTransform = CalibrationObject.transform;
-        CalibrationObjectTransform.position = CalibrationPoints[0];  // Set the first position
+        CalibrationObjectTransform = CalibrationObject.transform;    // Use to set the pre-defined calibration positions
+        CalibrationObjectTransform.position = new Vector3(CalibrationPoints[0,0], CalibrationPoints[0, 1], CalibrationPoints[0, 2] );  // Set the first position
         textStatus.text = "Step " + (currStep + 1);
+
+        // Testing space
+        GetCalibration( CalibrationPoints, CalibrationPoints );
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetKeyDown( KeyCode.Space ) && (currStep < numData))
-         {
+        {
             if (!initialAlignment)
             {
                 initialAlignment = true;
             }
             Vector3 relativePos = AlignmentObjectTransform.position - AlignmentObjectHMDTransform.position;
-            Quaternion relativeRot = Quaternion.Inverse( AlignmentObjectHMDTransform.rotation) * AlignmentObjectTransform.rotation; // rotation from HMD to object
+            Quaternion relativeRot = Quaternion.Inverse( AlignmentObjectHMDTransform.rotation ) * AlignmentObjectTransform.rotation; // rotation from HMD to object
 
             AlignmentPoints[currStep, 0] = relativePos.x;
             AlignmentPoints[currStep, 1] = relativePos.y;
@@ -82,12 +86,13 @@ public class SPAAMCalibration : MonoBehaviour
             else
             {
                 currStep++;
-                CalibrationObjectTransform.position = CalibrationPoints[currStep];  // Set next position
+                CalibrationObjectTransform.position = new Vector3( CalibrationPoints[currStep, 0], CalibrationPoints[currStep, 1], CalibrationPoints[currStep, 2] );  // Set next position
                 textStatus.text = "Step " + (currStep + 1);
             }
-        } else if (Input.GetKeyDown( KeyCode.P ))
+        }
+        else if (Input.GetKeyDown( KeyCode.P ))
         {
-            for (int i = 0; i < currStep; i ++)
+            for (int i = 0; i < currStep; i++)
             {
                 Debug.Log( "posx " + AlignmentPoints[i, 0].ToString( "F4" ) +
                             " posy " + AlignmentPoints[i, 1].ToString( "F4" ) +
@@ -99,8 +104,8 @@ public class SPAAMCalibration : MonoBehaviour
             }
         }
 
-        float xAxisValue = speed*Input.GetAxis( "Horizontal" );
-        float zAxisValue = speed*Input.GetAxis( "Vertical" );
+        float xAxisValue = speed * Input.GetAxis( "Horizontal" );
+        float zAxisValue = speed * Input.GetAxis( "Vertical" );
         if (Camera.current != null)
         {
             Camera.current.transform.Translate( new Vector3( xAxisValue, 0.0f, zAxisValue ) );
@@ -113,9 +118,41 @@ public class SPAAMCalibration : MonoBehaviour
         }
     }
 
-    private Matrix4x4 GetCalibration(float[,] measured, float[,] displayed)
+    /// <summary>
+    /// Get Calibration calculates the projection matrix from points in displayed to points in measured. The problem is defined
+    /// as qi = T*pi. If we rework the matrix we can get A*t = 0. The reworked matrix is of the following form:
+    /// A = [-q1 -q2 -q3 -1   0   0   0  0   0   0   0  0 p1q1 p1q2 p1q3;
+    ///        0   0   0  0 -q1 -q2 -q3 -1   0   0   0  0 p2q1 p2q2 p2q3;
+    ///        0   0   0  0   0   0   0  0 -q1 -q2 -q3 -1 p3q1 p3q2 p3q3;
+    ///        ...
+    ///        ]
+    /// and t is of the form:
+    /// t = [t11; t12; t13; t14; t21; t22; t23; t24; t31; t32; t33; t34; t41; t42; t43]
+    /// We solve for t using SVD decomposition.
+    /// </summary>
+    /// <param name="pi"></param>
+    /// <param name="qi"></param>
+    /// <returns></returns>
+    private Matrix4x4 GetCalibration( float[,] pi, float[,] qi )
     {
-        // TODO
+        Matrix A = Matrix.Build.Dense( 12 * 3, 15, 0 ); // Initialized a matrix of 36x15 of zeros
+        for (int p = 0; p < 12; p++)
+        {
+            Vector currRow1 = Vector.Build.Dense( new double[] { -qi[p, 0], -qi[p, 1], -qi[p, 2], -1, 0, 0, 0, 0, 0, 0, 0, 0, pi[p, 0] * qi[p, 0], pi[p, 0] * qi[p, 1], pi[p, 0] * qi[p, 2] } );
+            Vector currRow2 = Vector.Build.Dense( new double[] { 0, 0, 0, 0, -qi[p, 0], -qi[p, 1], -qi[p, 2], -1, 0, 0, 0, 0, pi[p, 1] * qi[p, 0], pi[p, 1] * qi[p, 1], pi[p, 1] * qi[p, 2] } );
+            Vector currRow3 = Vector.Build.Dense( new double[] { 0, 0, 0, 0, 0, 0, 0, 0, -qi[p, 0], -qi[p, 1], -qi[p, 2], -1, pi[p, 2] * qi[p, 0], pi[p, 2] * qi[p, 1], pi[p, 2] * qi[p, 2] } );
+            A.SetRow( 3 * p, currRow1 );
+            A.SetRow( 3 * p + 1, currRow2 );
+            A.SetRow( 3 * p + 2, currRow3 );
+        }
+
+        Debug.Log( A.ToString() );
+
+        var svd = A.Svd();
+
+        Debug.Log( "SVD" );
+        Debug.Log( (svd.VT).Column(14) );
+
         return new Matrix4x4();
     }
 
